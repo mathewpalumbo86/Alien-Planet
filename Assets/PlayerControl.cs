4ae6de6c24ac38e6f29111e772c2d96c89e0d1ae
Warpup <jonny.amacqueen@hotmail.com>
2016-04-01 07:27:08 +1100

    Base Project
    Very basic left right movement

---
+++ Assets/PlayerControl.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerControl : MonoBehaviour {
+    public float m_MovementSpeed;
+
+
+    void Awake()
+    {
+        m_MovementSpeed = 4f;
+    }
+	void Update () 
+    {
+        Movement();
+	}
+    void Movement()
+    {
+        if (Input.GetAxis("Horizontal") > 0)
+        {
+            transform.Translate(Vector2.right * m_MovementSpeed * Time.deltaTime);
+            transform.eulerAngles = new Vector2(0, 0);
+        }
+
+        if (Input.GetAxis("Horizontal") < 0)
+        {
+            transform.Translate(Vector2.right * m_MovementSpeed * Time.deltaTime);
+            transform.eulerAngles = new Vector2(0, 180);
+        }
+
+    }
+}
