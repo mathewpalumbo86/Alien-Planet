671cebb58d4138014c03d9f385ae0270c78e032d
Warpup <jonny.amacqueen@hotmail.com>
2016-04-03 02:08:55 +1000

    Basic item code
    Simply modifies the player speed and removes itself

---
+++ Assets/PickupItem.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PickupItem : MonoBehaviour {
+
+    public PlayerControl m_PlayerControl;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            
+            m_PlayerControl.m_MovementSpeed ++;
+            Debug.Log(m_PlayerControl.m_MovementSpeed);
+            Destroy(gameObject);
+        }
+        
+    }
+}
