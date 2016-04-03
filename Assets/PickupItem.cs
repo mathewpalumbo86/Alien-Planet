01310203adad827fb0e6ffcf9fbb5590807f16f1
Warpup <jonny.amacqueen@hotmail.com>
2016-04-03 19:26:31 +1000

    Testing

--- Assets/PickupItem.cs
+++ Assets/PickupItem.cs
@@ -9,6 +9,7 @@ public class PickupItem : MonoBehaviour {
     {
         if (other.tag == "Player")
         {
+            
             //test code
             m_PlayerControl.m_MovementSpeed ++;
             Debug.Log(m_PlayerControl.m_MovementSpeed);
